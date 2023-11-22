a71a00297dd1d5ed78bc73de762574b23bbc6be1
nan.chen <kevin-chen@foxmail.com>
2023-11-22 14:37:32 +0800

    x
    x

--- dist/breakup.js
+++ dist/breakup.js
@@ -64,7 +64,10 @@ class BreakUp {
                         let frame = config.res[name];
                         let tile = new bitmap_1.Bitmap(frame.sourceW, frame.sourceH);
                         tile.draw(bitmap, { x: frame.x, y: frame.y, width: frame.w, height: frame.h }, { x: frame.offX, y: frame.offY }, 0, 0);
-                        tile.save(`${folder}/${name}`);
+                        let ext = path.extname(name);
+                        if (!ext)
+                            ext = ".png";
+                        tile.save(`${folder}/${name}${ext}`);
                     }
                     break;
                 case "egret":
